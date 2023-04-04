c81345814b079855900b2f9200555fdc8115c8e2
maneesh5396 <manishkp16@gmail.com>
2023-04-05 00:04:03 +0530

    initial commit

--- lecture1/script.js
+++ lecture1/script.js
@@ -1,9 +1,25 @@
 console.log("Hello World...!!!");
 
-echo "# MyJavaScript" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/maneesh5396/MyJavaScript.git
-git push -u origin main
+// console.log("------------before ES6 or 2015-------");
+// var name = "maneesh";
+// var age = 26;
+// var gender = 'M';
+// var canCode = 'true';
+// console.log(name, age, gender, canCode);
+
+console.log("------------after ES6 or 2015-------");
+// with "let" -----> we can re-assign the value
+let firstname = "maneesh";
+let age = 26;
+let gender = 'M';
+let canCode = 'true';
+let lastname = "maneesh";
+console.log(firstname, age, gender, canCode);
+
+// with "Const" ------> we can't re-assign the value
+// const name = "maneesh";
+// const age = 26;
+// const gender = 'M';
+// const canCode = 'true';
+// const name = "manish"
+// console.log(name, age, gender, canCode);
