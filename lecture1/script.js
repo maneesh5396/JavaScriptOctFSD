39518e4b80b8414ec35eaad1ee5a1d29062f276c
maneesh5396 <manishkp16@gmail.com>
2023-04-04 22:15:38 +0530

    initial commit

---
+++ lecture1/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World...!!!");
+
+echo "# MyJavaScript" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/maneesh5396/MyJavaScript.git
+git push -u origin main
